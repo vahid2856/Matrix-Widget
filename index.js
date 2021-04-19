8a7136c04fd1e255df8244e64c8318fb2d8a5769
vahidnadarkhani <vahid.nadarkhani@gmail.com>
2021-04-19 22:50:45 +0430

    add node_modules=(matrix-js-sdk) and use it in the index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+import sdk from "./node_modules/matrix-js-sdk";
+
+// const client = sdk.createClient("https://matrix.org");
+
+var sdk = require("matrix-js-sdk");
+// client.login("m.login.password", {"user": "user", "password": "password"}).then((response) => {
+    
+//     const accessToken = response.access_token;
+// });
+// console.log(accessToken);
+
+const accessToken = "MDAxZGxvY2F0aW9uIHF1cmFuaWMubmV0d29yawowMDEzaWRlbnRpZmllciBrZXkKMDAxMGNpZCBnZW4gPSAxCjAwMzRjaWQgdXNlcl9pZCA9IEB2YWhpZF9uYWRhcmtoYW5pOnF1cmFuaWMubmV0d29yawowMDE2Y2lkIHR5cGUgPSBhY2Nlc3MKMDAyMWNpZCBub25jZSA9IGppUjI9Jk1UQTg3N1RuR3IKMDAyZnNpZ25hdHVyZSDcGkvyhbQ3NPmM1bTQGg4qw4QNIavaj7Di-sUOq54"
+
+const client = sdk.createClient({
+    baseUrl: "https://quranic.network",
+    accessToken: accessToken,
+    userId: "@vahid_nadarkhani:quranic.network"
+});
