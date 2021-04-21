d6174ba97def96c9557bbe908f42e8553f82dbda
vahidnadarkhani <vahid.nadarkhani@gmail.com>
2021-04-21 14:31:58 +0430

    change index.js and use username and password for get accessToken from client

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 //import sdk from "./node_modules/matrix-js-sdk";
-
-// const client = sdk.createClient("https://matrix.org");
-
 var sdk = require("matrix-js-sdk");
-// client.login("m.login.password", {"user": "user", "password": "password"}).then((response) => {
+console.log(sdk)
+console.log("--------------------------");
+
+
+const client = sdk.createClient("https://quranic.network");
+
+console.log(client)
+console.log("--------------------------");
+
+client.login("m.login.password", {"user": "@vahid_nadarkhani:quranic.network", "password": "invalidP1988"}).then((response) => {
     
-//     const accessToken = response.access_token;
-// });
-// console.log(accessToken);
-
-const accessToken = "MDAxZGxvY2F0aW9uIHF1cmFuaWMubmV0d29yawowMDEzaWRlbnRpZmllciBrZXkKMDAxMGNpZCBnZW4gPSAxCjAwMzRjaWQgdXNlcl9pZCA9IEB2YWhpZF9uYWRhcmtoYW5pOnF1cmFuaWMubmV0d29yawowMDE2Y2lkIHR5cGUgPSBhY2Nlc3MKMDAyMWNpZCBub25jZSA9IGppUjI9Jk1UQTg3N1RuR3IKMDAyZnNpZ25hdHVyZSDcGkvyhbQ3NPmM1bTQGg4qw4QNIavaj7Di-sUOq54pawo"
-
-const client = sdk.createClient({
-    baseUrl: "https://quranic.network",
-    accessToken: accessToken,
-    userId: "@vahid_nadarkhani:quranic.network"
+    const accessToken = response.access_token;
 });
+
+console.log(accessToken);
+console.log("--------------------------");
+
+// const accessToken = "MDAxZGxvY2F0aW9uIHF1cmFuaWMubmV0d29yawowMDEzaWRlbnRpZmllciBrZXkKMDAxMGNpZCBnZW4gPSAxCjAwMzRjaWQgdXNlcl9pZCA9IEB2YWhpZF9uYWRhcmtoYW5pOnF1cmFuaWMubmV0d29yawowMDE2Y2lkIHR5cGUgPSBhY2Nlc3MKMDAyMWNpZCBub25jZSA9IGppUjI9Jk1UQTg3N1RuR3IKMDAyZnNpZ25hdHVyZSDcGkvyhbQ3NPmM1bTQGg4qw4QNIavaj7Di-sUOq54pawo"
+
+// const client = sdk.createClient({
+//     baseUrl: "https://quranic.network",
+//     accessToken: accessToken,
+//     userId: "@vahid_nadarkhani:quranic.network"
+// });
