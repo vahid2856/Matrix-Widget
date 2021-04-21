f65bc28b3b0f6bf2f65c122ef5bb66d59b07985c
vahidnadarkhani <vahid.nadarkhani@gmail.com>
2021-04-21 14:21:08 +0430

    convert index.js to bundle.js with browserify

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import sdk from "./node_modules/matrix-js-sdk";
+//import sdk from "./node_modules/matrix-js-sdk";
 
 // const client = sdk.createClient("https://matrix.org");
 
@@ -9,7 +9,7 @@ var sdk = require("matrix-js-sdk");
 // });
 // console.log(accessToken);
 
-const accessToken = "MDAxZGxvY2F0aW9uIHF1cmFuaWMubmV0d29yawowMDEzaWRlbnRpZmllciBrZXkKMDAxMGNpZCBnZW4gPSAxCjAwMzRjaWQgdXNlcl9pZCA9IEB2YWhpZF9uYWRhcmtoYW5pOnF1cmFuaWMubmV0d29yawowMDE2Y2lkIHR5cGUgPSBhY2Nlc3MKMDAyMWNpZCBub25jZSA9IGppUjI9Jk1UQTg3N1RuR3IKMDAyZnNpZ25hdHVyZSDcGkvyhbQ3NPmM1bTQGg4qw4QNIavaj7Di-sUOq54"
+const accessToken = "MDAxZGxvY2F0aW9uIHF1cmFuaWMubmV0d29yawowMDEzaWRlbnRpZmllciBrZXkKMDAxMGNpZCBnZW4gPSAxCjAwMzRjaWQgdXNlcl9pZCA9IEB2YWhpZF9uYWRhcmtoYW5pOnF1cmFuaWMubmV0d29yawowMDE2Y2lkIHR5cGUgPSBhY2Nlc3MKMDAyMWNpZCBub25jZSA9IGppUjI9Jk1UQTg3N1RuR3IKMDAyZnNpZ25hdHVyZSDcGkvyhbQ3NPmM1bTQGg4qw4QNIavaj7Di-sUOq54pawo"
 
 const client = sdk.createClient({
     baseUrl: "https://quranic.network",
