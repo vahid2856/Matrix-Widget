fb7f08a6c9c0a42ac39998c4434bcf51b8ebca78
vahidnadarkhani <vahid.nadarkhani@gmail.com>
2021-04-21 14:38:40 +0430

    add bundle.js

--- bundle.js
+++ bundle.js
@@ -1,22 +1,31 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 //import sdk from "./node_modules/matrix-js-sdk";
-
-// const client = sdk.createClient("https://matrix.org");
-
+console.log("Start");
 var sdk = require("matrix-js-sdk");
-// client.login("m.login.password", {"user": "user", "password": "password"}).then((response) => {
+console.log(sdk);
+console.log("--------------------------");
+
+
+const client = sdk.createClient("https://quranic.network");
+
+console.log(client);
+console.log("--------------------------");
+
+client.login("m.login.password", {"user": "@vahid_nadarkhani:quranic.network", "password": "invalidP1988"}).then((response) => {
     
-//     const accessToken = response.access_token;
-// });
-// console.log(accessToken);
-
-const accessToken = "MDAxZGxvY2F0aW9uIHF1cmFuaWMubmV0d29yawowMDEzaWRlbnRpZmllciBrZXkKMDAxMGNpZCBnZW4gPSAxCjAwMzRjaWQgdXNlcl9pZCA9IEB2YWhpZF9uYWRhcmtoYW5pOnF1cmFuaWMubmV0d29yawowMDE2Y2lkIHR5cGUgPSBhY2Nlc3MKMDAyMWNpZCBub25jZSA9IGppUjI9Jk1UQTg3N1RuR3IKMDAyZnNpZ25hdHVyZSDcGkvyhbQ3NPmM1bTQGg4qw4QNIavaj7Di-sUOq54pawo"
-
-const client = sdk.createClient({
-    baseUrl: "https://quranic.network",
-    accessToken: accessToken,
-    userId: "@vahid_nadarkhani:quranic.network"
+    const accessToken = response.access_token;
 });
+
+console.log(accessToken);
+console.log("--------------------------");
+
+// const accessToken = "MDAxZGxvY2F0aW9uIHF1cmFuaWMubmV0d29yawowMDEzaWRlbnRpZmllciBrZXkKMDAxMGNpZCBnZW4gPSAxCjAwMzRjaWQgdXNlcl9pZCA9IEB2YWhpZF9uYWRhcmtoYW5pOnF1cmFuaWMubmV0d29yawowMDE2Y2lkIHR5cGUgPSBhY2Nlc3MKMDAyMWNpZCBub25jZSA9IGppUjI9Jk1UQTg3N1RuR3IKMDAyZnNpZ25hdHVyZSDcGkvyhbQ3NPmM1bTQGg4qw4QNIavaj7Di-sUOq54pawo"
+
+// const client = sdk.createClient({
+//     baseUrl: "https://quranic.network",
+//     accessToken: accessToken,
+//     userId: "@vahid_nadarkhani:quranic.network"
+// });
 
 },{"matrix-js-sdk":24}],2:[function(require,module,exports){
 function _defineProperty(obj, key, value) {
